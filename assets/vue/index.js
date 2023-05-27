57b705ad4bcdc9c0fcca4bdb775623ccb01bcfba
ErrorDoc404 <akashsinhakvs@gmail.com>
2023-05-27 09:42:26 +0530

    update vue index file

--- assets/vue/index.js
+++ assets/vue/index.js
@@ -1,14 +1,34 @@
 var app = new Vue({
     el: '#roster',
-    data() {
-        return {
-            roster: [],
-        }
+    data: {
+      roster: [],
+      loading: false,
+      error: null
     },
-    created() {
+    created: function() {
+      this.fetchRoster();
+    },
+    methods: {
+      fetchRoster: function() {
+        this.loading = true;
+        var self = this; // Store reference to the Vue instance
+  
         fetch('/api/roster')
-            .then(response => response.json())
-            .then(data => (this.roster = data))
-            .then(data => console.log(roster))
+          .then(function(response) {
+            if (!response.ok) {
+              throw new Error('Failed to fetch roster');
+            }
+            return response.json();
+          })
+          .then(function(data) {
+            self.roster = data;
+            self.loading = false;
+          })
+          .catch(function(error) {
+            self.error = 'An error occurred while fetching the roster.';
+            self.loading = false;
+            console.error(error);
+          });
+      }
     }
 });
